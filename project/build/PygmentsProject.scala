bbfd3926dc4f8abed836783b23b3a8c45ebaf18a
Bart Schuller <schuller@lunatech.com>
2011-04-29 11:30:05 +0200

    Released version 0.1.0

--- project/build/PygmentsProject.scala
+++ project/build/PygmentsProject.scala
@@ -5,4 +5,8 @@ class PygmentsProject(info: ProjectInfo) extends DefaultProject(info) {
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.7.2" % "test->default"
 
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
+
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Lunatech Public" at "http://nexus.lunatech.com/content/repositories/lunatech-public/"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
